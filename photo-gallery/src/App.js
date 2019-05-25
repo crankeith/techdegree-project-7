514a6d22fcf931289cfb6e9198580772fd9f167c
Andrew Keith <38077188+crankeith@users.noreply.github.com>
2019-05-25 00:57:15 -0400

    Add axios call to Flickr API

--- photo-gallery/src/App.js
+++ photo-gallery/src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
+import apiKey from './config.js'
 
 import SearchForm from './components/SearchForm'
 import Nav from './components/Nav'
 import Gallery from './components/Gallery'
 
-function App() {
-  return (
-    <div className="container">
-      <SearchForm />
-      <Nav />
-      <Gallery />
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        searchResults:[]
+    };
+
+    componentDidMount() {
+        //TODO update variable with search text from form
+        const searchText = "cat";
+        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=${apiKey}&text=${searchText}`)
+            .then(response => {
+                console.log(response.data.photos.photo)
+                this.setState({
+                    searchResults: response.data.photos.photo
+                })
+            })
+    }
+
+    render(){
+      return (
+        <div className="container">
+          <SearchForm />
+          <Nav />
+          <Gallery photos={this.state.searchResults} />
+        </div>
+      );
+    }
 }
 
 export default App;
--- photo-gallery/src/components/Gallery.js
+++ photo-gallery/src/components/Gallery.js
@@ -2,23 +2,20 @@ import React from 'react';
 
 import NotFound from './NotFound'
 
-function Gallery(){
+function Gallery(props){
     return(
         <div className="photo-container">
             <h2>Results</h2>
             <ul>
-                <li>
-                    <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt=""/>
-                </li>
-                <li>
-                    <img src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg" alt=""/>
-                </li>
-                <li>
-                    <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt=""/>
-                </li>
-                <li>
-                    <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt=""/>
-                </li>
+                {props.photos.map( photo => {
+                    return (
+                        <li>
+                            <a target="_blank" href={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}>
+                                <img src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} alt=""/>
+                            </a>
+                        </li>
+                    )
+                })}
                 <NotFound/>
             </ul>
         </div>
